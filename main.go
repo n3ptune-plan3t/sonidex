fff13d55536bc17f21625fdf0765596e62277bab
n3ptune-plan3t <plawanjyotitalukdar@gmail.com>
2026-09-03 22:17:21 +0530

    Remove unused imports from main.go
    Removed unused imports for image/color and fyne.io/fyne/v2/canvas.

--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
+	_ "embed"
 	"context"
 	"fmt"
-	"image/color"
 	"sync"
 	"time"
 
-	_ "embed"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 
